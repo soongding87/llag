30eeed3c2b487680391b7cdb79a2a211f15a0a11
Cedric Chou <cdric.chou@gmail.com>
2018-06-22 16:28:25 -0700

    passport local strategy implemented

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const keys = require("./config/app_keys");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const User = mongoose.model("User");
 
 const index = require("./routes/index");
 const users = require("./routes/users");
@@ -15,20 +16,65 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// authentication libraries
+
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const session = require("express-session");
+const LocalStrategy = require("passport-local").Strategy;
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// session
+
+app.use(
+  session({
+    secret: "supersecret",
+    resave: false,
+    store: store,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// controllers
+
 app.use("/", index);
-app.use("/users", users);
+app.use("/api/users", users);
+app.use("/api/login", login);
+app.use("/api/logout", logout);
+
+// passport local strategy
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    User.findOne({ username }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      // const hash = user.password;
+      // bcrypt.compare(password, hash, function(err, res) {
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+      return done(null, user);
+      // });
+    });
+  })
+);
 
 const PORT = 4000;
 app.listen(PORT);
