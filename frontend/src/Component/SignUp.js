a6d8ae16e9d7450559475d329ed520fcf1e02028
Cedric Chou <cdric.chou@gmail.com>
2018-06-22 17:11:20 -0700

    sign up working and saving info in database

--- frontend/src/Component/SignUp.js
+++ frontend/src/Component/SignUp.js
@@ -1,65 +1,66 @@
-import React ,{ Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import axios from 'axios';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 const styles = theme => ({
   container: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap"
   },
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: 200,
+    width: 200
   },
   menu: {
-    width: 200,
-  },
+    width: 200
+  }
 });
-
 
 class SignUp extends Component {
   constructor(props) {
-    super()
+    super();
     this.state = {
-      username: '',
-      password: ''
-    }
+      username: "",
+      password: ""
+    };
   }
 
   handleChange = name => event => {
- this.setState({
-   [name]: event.target.value,
- });
-};
+    this.setState({
+      [name]: event.target.value
+    });
+  };
 
-handleSubmit = event => {
-  event.preventDefault();
-  axios.post("/users", this.state).then(function(res){
-    console.log("submit is working")
-    res.redirect("/");
-  })
-}
+  handleSubmit = event => {
+    event.preventDefault();
+    axios.post("/api/users", this.state).then(function(res) {
+      console.log("submit is working");
+      res.redirect("/");
+    });
+  };
 
   render() {
     const { classes } = this.props;
 
-
-
     return (
-      <form className={classes.container} noValidate autoComplete="off" onSubmit={this.handleSubmit}>
+      <form
+        className={classes.container}
+        noValidate
+        autoComplete="off"
+        onSubmit={this.handleSubmit}
+      >
         <TextField
           required
           id="required"
           label="name"
           value={this.state.username}
           className={classes.textField}
-          onChange={this.handleChange('username')}
+          onChange={this.handleChange("username")}
           margin="normal"
         />
-
 
         <TextField
           id="password-input"
@@ -68,7 +69,7 @@ handleSubmit = event => {
           type="password"
           value={this.state.password}
           autoComplete="current-password"
-          onChange={this.handleChange('password')}
+          onChange={this.handleChange("password")}
           margin="normal"
         />
         <button type="submit">submit</button>
