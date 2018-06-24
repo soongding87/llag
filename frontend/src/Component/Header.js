4bd6ea604e65819fd7cbfe99bb04b779e15665ca
Taehoon Daniel Kang <kangtaehoon87@gmail.com>
2018-06-23 17:00:44 -0700

    Change CSS style to reactstrap, Change Navbar , Add router

---
+++ frontend/src/Component/Header.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="info" light expand="md">
+          <NavbarBrand href="/">LLAG</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/Signup">Sign up</NavLink>
+              </NavItem>
+
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
