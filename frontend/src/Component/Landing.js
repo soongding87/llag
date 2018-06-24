df14494455629949534e05377cf203bf40116031
Taehoon Daniel Kang <kangtaehoon87@gmail.com>
2018-06-23 23:44:06 -0700

    add animate.css and changed landing page

--- frontend/src/Component/Landing.js
+++ frontend/src/Component/Landing.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 
+
 const Landing =()=>{
   return(
-   <div>
-     this is landing page
+   <div className="animated tada ">
+     <h1 style={{textAlign:'center'}}>Welcome to LLAG </h1>
+     <h2 style={{textAlign:'center'}}> This is education platform for everyone </h2>
    </div>
  )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Component/App';
 import 'semantic-ui-css/semantic.min.css';
-
+import 'animate.css/animate.min.css';
+import 'typeface-roboto';
 
 ReactDOM.render(<App />, document.getElementById('root'));
