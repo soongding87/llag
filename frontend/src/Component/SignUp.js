8fd59ea90f91a1f160ec48145cb213cbb724441d
Taehoon Daniel Kang <kangtaehoon87@gmail.com>
2018-06-26 16:41:32 -0700

    fixed merge files

--- frontend/src/Component/SignUp.js
+++ frontend/src/Component/SignUp.js
@@ -52,33 +52,7 @@ class SignUp extends Component {
           <Button>Submit</Button>
         </Form>
       </div>
-      // <form>
-      //   className={classes.container}
-      //   noValidate
-      //   autoComplete="off"
-      //   onSubmit={this.handleSubmit}
-      // >
-      //   <TextField
-      //     required
-      //     id="required"
-      //     label="name"
-      //     value={this.state.username}
-      //     className={classes.textField}
-      //     onChange={this.handleChange("username")}
-      //     margin="normal"
-      //   />
-      //
-      //   <TextField
-      //     id="password-input"
-      //     label="Password"
-      //     className={classes.textField}
-      //     type="password"
-      //     value={this.state.password}
-      //     autoComplete="current-password"
-      //     onChange={this.handleChange("password")}
-      //     margin="normal"
-      //   />
-      // </form>
+      
     );
   }
 }
