96d266e1ab685248604eb124ab2adc8d17e1cc59
Taehoon Daniel Kang <kangtaehoon87@gmail.com>
2018-06-26 16:45:15 -0700

    fixed some github error

--- frontend/src/Component/Landing.js
+++ frontend/src/Component/Landing.js
@@ -1,16 +1,5 @@
 import React from 'react';
 
-const landingStyle = {
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'cover',
-  backgroundImage:
-    'url(https://images.pexels.com/photos/926987/pexels-photo-926987.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)',
-  height: '60vh',
-  alignItems: 'center',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center'
-};
 
 const landingStyle = {
   backgroundRepeat: 'no-repeat',
@@ -23,16 +12,7 @@ const landingStyle = {
   justifyContent: 'center'
 }
 
-const Landing =()=>{
-  return(
-   <div className="container" style={landingStyle}>
-     <div className="animated fadeInDown">
-     <h1 style={{textAlign:'center'}}>Welcome to LLAG </h1>
-     <h2 style={{textAlign:'center'}}> This is education platform for everyone </h2>
-   </div>
- </div>
- )
-}
+  
 
 const Landing = () => {
   return (
