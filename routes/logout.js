4314c04e5591003b15a9f8c005303cfae779c069
Cedric Chou <cdric.chou@gmail.com>
2018-07-03 16:57:40 -0700

    logout working on backend

--- routes/logout.js
+++ routes/logout.js
@@ -5,12 +5,10 @@ const passport = require("passport");
 
 module.exports = app => {
   app.get("/api/logout", function(req, res, next) {
-    console.log("try logout", req.session)
     req.logout();
     req.session.destroy();
     res.json({
       message: "Logged out"
     });
   });
-
 };
