fcb367ef3ce9e08dc9f6c2930a1cc2153988cab7
Cedric Chou <cdric.chou@gmail.com>
2018-07-12 14:12:03 -0700

    added ...loading between state loading

--- frontend/src/Component/App.js
+++ frontend/src/Component/App.js
@@ -9,23 +9,42 @@ import Landing from "./Landing";
 import Contents from "./Contents";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      render: false
+    };
+  }
+
   componentDidMount() {
     this.props.fetchUser();
+    setTimeout(() => {
+      this.setState({ render: true });
+    }, 300);
   }
+
   render() {
-    return (
-      <div className="ui container">
-        <Header />
-        <BrowserRouter>
-          <div>
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/signin" component={SignIn} />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/contents" component={Contents} />
-          </div>
-        </BrowserRouter>
-      </div>
-    );
+    if (this.state.render === false) {
+      return (
+        <div>
+          <h2>...Loading</h2>
+        </div>
+      );
+    } else {
+      return (
+        <div className="ui container">
+          <Header />
+          <BrowserRouter>
+            <div>
+              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/signin" component={SignIn} />
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/contents" component={Contents} />
+            </div>
+          </BrowserRouter>
+        </div>
+      );
+    }
   }
 }
 
--- frontend/src/reducers/authReducer.js
+++ frontend/src/reducers/authReducer.js
@@ -1,7 +1,6 @@
-
 export default function(state = null, action) {
   switch (action.type) {
-    case 'fetch_user':
+    case "fetch_user":
       return action.payload || false;
     default:
       return state;
