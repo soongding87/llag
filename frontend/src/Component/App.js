406b395131e9dfc72d933111cec09b2aa1e09d2a
Taehoon Daniel Kang <kangtaehoon87@gmail.com>
2018-07-12 16:22:16 -0700

    Client (#24)
    * small modification
    
    * remove unecessary files
    
    * current user object returned as json
    
    * logout working on backend
    
    * NavBar partly fixed based on sign-in sign-out user
    
    * added ...loading between state loading
    
    * added footer and localstorage
    
    * footer added

--- frontend/src/Component/App.js
+++ frontend/src/Component/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import * as actions from "../actions";
 import Header from "./Header";
+import Footer from "./Footer";
+
 import Signup from "./auth/Signup";
 import Signin from "./auth/Signin";
 import { BrowserRouter, Route } from "react-router-dom";
@@ -8,6 +10,8 @@ import { connect } from "react-redux";
 import Landing from "./Landing";
 import Contents from "./Contents";
 import Signout from "./auth/Signout";
+import "../styling/app.css";
+
 
 class App extends Component {
   render() {
